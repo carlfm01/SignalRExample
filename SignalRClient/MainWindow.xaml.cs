475d86b0eee137e81137d5345bd8a8d21291e358
Carlos Fonseca M <carlfm01@hotmail.com>
2017-10-30 22:17:22 -0600

    Chat example

--- SignalRClient/MainWindow.xaml.cs
+++ SignalRClient/MainWindow.xaml.cs
@@ -21,15 +21,22 @@ namespace SignalRClient
         {
             try
             {
+                //cambiar puerto para pruebas locales
+                //o dirección del servidor remoto
                 string serverUrl = "http://localhost:17426/signalr";
                 _hubConnection = new HubConnection(serverUrl);
                 _hubProxy = _hubConnection.CreateHubProxy("NotificationHub");
                 _hubProxy.On<string>("NotifyClients", (message) =>
                 {
                     //message => mensaje opcional
-                    MessageBox.Show("Cliente notificado");
+                   
+                    Dispatcher.Invoke(() => lbxMessages.Items.Add($"Cliente remoto - {message}"));
+                   
                 });
+                _hubConnection.StateChanged += _hubConnection_StateChanged;
+
                 await _hubConnection.Start();
+                btnReconnect.Visibility = Visibility.Collapsed;
             }
             catch (System.Exception ex)
             {
@@ -40,26 +47,57 @@ namespace SignalRClient
 
         }
 
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void _hubConnection_StateChanged(StateChange obj)
         {
+            switch (obj.NewState)
+            {
+                case ConnectionState.Connecting:
+                    break;
+                case ConnectionState.Connected:
+                    MessageBox.Show("Conectado al servidor");
+                    break;
+                case ConnectionState.Reconnecting:
+                    break;
+                case ConnectionState.Disconnected:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btnReconnect_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectSignalRAsync();
+        }
+
+        private void btnClients_Click(object sender, RoutedEventArgs e)
+        {
+            //mensaje a enviar a los clientes
+            string message = txtMessage.Text;
             try
             {
-                _hubProxy.Invoke("NotifyServer");
+                if (string.IsNullOrWhiteSpace(message)) 
+                    MessageBox.Show("No se pueden enviar mensajes en blanco");
+                else
+                {
+                    _hubProxy.Invoke("NotifyClients", message);
+                    lbxMessages.Items.Add($"Tú - {message}");
+                    txtMessage.Text = string.Empty;
+                }
+                    
             }
             catch (System.Exception ex)
             {
                 MessageBox.Show(ex.ToString());
 
             }
-
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnServer_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                _hubProxy.Invoke("NotifyClients", "Mensaje opcional");
+                _hubProxy.Invoke("NotifyServer");
             }
             catch (System.Exception ex)
             {
